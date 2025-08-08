b16fc6ba5e893af2951f099c286a0661882206b2
SJ <theuncutspace@gmail.com>
2025-08-09 04:25:10 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
     domains: ['srxjkxxuhulqpmtxedbl.supabase.co'],
   },
-  // PWA Configuration
   async headers() {
     return [
       {
